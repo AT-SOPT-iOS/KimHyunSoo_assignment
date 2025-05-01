54dfdedae2b92a56f1494f756a01fa3aa3866e7d
maeng-kim <ehseo0410@gmail.com>
2025-05-01 23:52:11 +0900

    [feat/#3] 야구 구단 목록 UI

---
+++ ATSOPT_Assignment/ATSOPT_Assignment/Presentation/Model/BaseballTeamModel.swift
@@ -0,0 +1,25 @@
+//
+//  BaseballTeamModel.swift
+//  ATSOPT_Assignment
+//
+//  Created by MaengKim on 5/1/25.
+//
+
+import UIKit
+
+struct BaseballTeamModel {
+    let image: UIImage
+}
+
+extension BaseballTeamModel {
+    static func dummy() -> [BaseballTeamModel] {
+        return [
+            BaseballTeamModel(image: .bears),
+            BaseballTeamModel(image: .lions),
+            BaseballTeamModel(image: .kt),
+            BaseballTeamModel(image: .nc),
+            BaseballTeamModel(image: .eagles),
+            BaseballTeamModel(image: .giants)
+        ]
+    }
+}
